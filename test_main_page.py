38b5bf267b18a4f259677385b95e4b9b8741a896
yushkinv <yvv80@mail.ru>
2022-02-02 16:37:58 +0300

    added element verification on the page

--- test_main_page.py
+++ test_main_page.py
@@ -6,3 +6,9 @@ def test_guest_can_go_to_login_page(browser):
                                    # экземпляр драйвера и url адрес
     page.open()                    # открываем страницу
     page.go_to_login_page()        # выполняем метод страницы — переходим на страницу логина
+
+def test_guest_should_be_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
